0127e3532a5cd78a32e1a52e6a2307c4063e68b9
Selmor <selmor_1@mail.ru>
2023-02-20 10:51:55 +0300

    Knights (ver.3)

--- knights.py
+++ knights.py
@@ -2,14 +2,13 @@ import time
 
 def zeroes_count(f):
     res = 0
-    for rw in indexes:
-        for cl in indexes:
-            if f[rw][cl] == 0:
-                res += 1
+    for i in range(CELLS_COUNT):
+        rw, cl = cells[i]
+        if f[rw][cl] == 0:
+            res += 1
     return res
 
 def dump_field(f):
-    print()
     # for rw in range(TOTAL_SIZE):
     #     print(*f[rw])
     # for rw in range(SIZE):
@@ -21,49 +20,56 @@ def dump_field(f):
             else:
                 print(f'{f[rw][cl]:^3}',end='')
         print()
-    # print(*f)
+    print()
 
 def set_knight(f, rw, cl, d):
     f[rw][cl] += d * 100
     for mv in moves:
         f[rw + mv[0]][cl + mv[1]] += d
 
-# поле представляет собой 2D-массив
-def scan_field(f, best, num = 0) -> int:
+def start_scan_field(f):
+    best = MAX_KNIGHTS + 1
+    for i in range(CELLS_COUNT):
+        print(f'...{i+1} / {CELLS_COUNT}')
+        res = scan_field(f, best, 0, 0)
+        best = min(best, res)
+    print(f'BEST KNIGHTS = {best}')
+
+def scan_field(f, best, ind = 0, num = 0) -> int:
     if num >= best:
         return best + 1
-
     if num >= FROM_NUM and zeroes_count(f) == 0: # found
+        print(f'KNIGHTS> {num}')
         dump_field(f)
         return num
-
     my_best = best
-    for rw in indexes:
-        for cl in indexes:
-            if f[rw][cl] < 100:
-                set_knight(f, rw, cl, +1) # add knights
-                scan_res = scan_field(f, my_best, num + 1)
-                set_knight(f, rw, cl, -1) # del knights
-                my_best = min(scan_res, my_best)
+    for i in range(ind, CELLS_COUNT):
+        rw, cl = cells[i]
+        if f[rw][cl] < 100:
+            set_knight(f, rw, cl, +1) # add knights
+            scan_res = scan_field(f, my_best, i + 1, num + 1)
+            set_knight(f, rw, cl, -1) # del knights
+            my_best = min(scan_res, my_best)
     return my_best
 
-SIZE = 5 # 8
+SIZE = 6 # 8
+CELLS_COUNT = SIZE * SIZE
 TOTAL_SIZE = SIZE + 4
-MAX_KNIGHTS = 9 # 13
+MAX_KNIGHTS = 10 # 13
 FROM_NUM = (SIZE * (SIZE + 1) - 1) // 9
-indexes = [i+2 for i in range(SIZE)]
-moves = [[-2, -1], [-2, +1], [+2, -1], [+2, +1], [-1, -2], [-1, +2], [+1 ,-2], [+1, +2]]
-f = []
+f = [] # поле представляет собой 2D-массив
 for _ in range(TOTAL_SIZE):
     l = [0 for i in range(TOTAL_SIZE)]
     f.append(l)
-
-# print(f'FROM_NUM = {FROM_NUM}')
-# dump_field(f)
+indexes = [i+2 for i in range(SIZE)]
+cells = list()
+for rw in indexes:
+    for cl in indexes:
+        cells.append((rw, cl))
+moves = ((-2, -1), (-2, +1), (+2, -1), (+2, +1), (-1, -2), (-1, +2), (+1 ,-2), (+1, +2))
 
 print('...started')
-
 start_time = time.perf_counter()
-print(scan_field(f, MAX_KNIGHTS + 1))
+start_scan_field(f)
 end_time = time.perf_counter()
 print(f"The execution time: {end_time - start_time:.8f} seconds")
