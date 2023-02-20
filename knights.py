7f157359eab3ad6bc5ac9f6926ebcbbd648fa23f
Selmor <selmor_1@mail.ru>
2023-02-20 10:13:47 +0300

    Knights (ver.2)

--- knights.py
+++ knights.py
@@ -1,36 +1,69 @@
-def address(index): #
-    return index % 8, index // 8
+import time
 
-def field_is_full(f):
-    return False
+def zeroes_count(f):
+    res = 0
+    for rw in indexes:
+        for cl in indexes:
+            if f[rw][cl] == 0:
+                res += 1
+    return res
 
 def dump_field(f):
-    print(*f)
+    print()
+    # for rw in range(TOTAL_SIZE):
+    #     print(*f[rw])
+    # for rw in range(SIZE):
+    #     print(*f[rw+2][2:2+SIZE])
+    for rw in indexes:
+        for cl in indexes:
+            if f[rw][cl] >= 100:
+                print(' ` ',end='')
+            else:
+                print(f'{f[rw][cl]:^3}',end='')
+        print()
+    # print(*f)
+
+def set_knight(f, rw, cl, d):
+    f[rw][cl] += d * 100
+    for mv in moves:
+        f[rw + mv[0]][cl + mv[1]] += d
 
 # поле представляет собой 2D-массив
-def scan_field(f, num = 1) -> int:
-    for ind in range(64):
-        # cl, rw = address(ind)
-        cl, rw = ind % 8, ind // 8
-        if f[rw][cl] < 0:
-            continue
-        f[rw][cl] = -1 * (f[rw][cl] + 1)
-        # add knights
-        if num == 1:
-            dump_field(f)
-        if num > 7 and field_is_full(f):
-            #  print field
-            return num
-        # if num < 13:
-        if num < 3:
-            scan_field(f, num + 1)
-        f[rw][cl] = -1 * f[rw][cl] - 1
+def scan_field(f, best, num = 0) -> int:
+    if num >= best:
+        return best + 1
 
+    if num >= FROM_NUM and zeroes_count(f) == 0: # found
+        dump_field(f)
+        return num
+
+    my_best = best
+    for rw in indexes:
+        for cl in indexes:
+            if f[rw][cl] < 100:
+                set_knight(f, rw, cl, +1) # add knights
+                scan_res = scan_field(f, my_best, num + 1)
+                set_knight(f, rw, cl, -1) # del knights
+                my_best = min(scan_res, my_best)
+    return my_best
+
+SIZE = 5 # 8
+TOTAL_SIZE = SIZE + 4
+MAX_KNIGHTS = 9 # 13
+FROM_NUM = (SIZE * (SIZE + 1) - 1) // 9
+indexes = [i+2 for i in range(SIZE)]
+moves = [[-2, -1], [-2, +1], [+2, -1], [+2, +1], [-1, -2], [-1, +2], [+1 ,-2], [+1, +2]]
 f = []
-for _ in range(8):
-    l = [0 for i in range(8)]
+for _ in range(TOTAL_SIZE):
+    l = [0 for i in range(TOTAL_SIZE)]
     f.append(l)
 
-scan_field(f)
-
+# print(f'FROM_NUM = {FROM_NUM}')
 # dump_field(f)
+
+print('...started')
+
+start_time = time.perf_counter()
+print(scan_field(f, MAX_KNIGHTS + 1))
+end_time = time.perf_counter()
+print(f"The execution time: {end_time - start_time:.8f} seconds")
